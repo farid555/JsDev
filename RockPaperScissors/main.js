33b4133451f4ac4b64d03d40322ea8dfe0f8437f
farid555 <farid555a@gmail.com>
2022-11-19 22:16:06 +0200

    Objects

--- RockPaperScissors/main.js
+++ RockPaperScissors/main.js
@@ -98,3 +98,95 @@ function myfunc() {
 }
 
 myfunc();
+
+//***Object***//
+
+const myObj = { name: "Mohammad" };
+console.log(myObj);
+
+const anotherObject = {
+  alive: true,
+  answer: 43,
+  hobbies: ["Eat", "Sleep", "Code"],
+  beverage: {
+    morning: "Cooffe",
+    afternoon: "Iced Tea",
+  },
+  action: function () {
+    return "Hello there!";
+  },
+  earlyMornin: function () {
+    return `Time for ${this.beverage.morning}`;
+  },
+};
+
+console.log(anotherObject.beverage.afternoon);
+console.log(anotherObject["beverage"]["morning"]);
+console.log(anotherObject.action());
+console.log(anotherObject.earlyMornin());
+
+//***Vehical Object***//
+
+const vehical = {
+  wheels: 4,
+  engine: function () {
+    return "Vrrooom!";
+  },
+};
+
+const truck = Object.create(vehical);
+truck.doors = 2;
+console.log(truck);
+console.log(truck.wheels); //** Inheritance */
+console.log(truck.engine());
+
+const car = Object.create(vehical);
+car.doors = 4;
+car.engine = function () {
+  return "Whooooosh!";
+};
+
+console.log(car.engine());
+console.log(car.wheels);
+
+const tesla = Object.create(car);
+console.log(tesla.wheels);
+
+tesla.engine = function () {
+  return "Shhhhhh...";
+};
+
+console.log(tesla.engine());
+
+//** Object key-value */
+
+const band = {
+  vocals: "Robert Plant",
+  guitar: "Jimmy Page",
+  bass: "Jhone Paul Jones",
+  drums: "John Bonham",
+};
+
+/delete band.drums; /; /**Delete the Drums Keys */
+console.log(band.hasOwnProperty("drums"));
+
+console.log(Object.keys(band)); //**Keys */
+console.log(Object.values(band)); //**properties */
+
+for (let job in band) {
+  console.log(`On ${job}, it's ${band[job]}!`); //**Keys and properties*/
+}
+
+//**destructuring Objects */
+
+const { guitar: myVariable, bass: myBass, vocals, drums } = band;
+console.log(myVariable);
+
+console.log(myBass);
+console.log(vocals);
+console.log(drums);
+
+function sings({ vocals }) {
+  return `${vocals} sings!`;
+}
+console.log(sings(band));
